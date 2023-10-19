ae5cfaabed8949409f73afd3e16c38b2751d97cd
hxhkim <hahyun.h.kim@gmail.com>
2023-10-19 11:54:55 +0900

    add func * run test

--- src/hello_hxhkim/who.py
+++ src/hello_hxhkim/who.py
@@ -1,2 +1,5 @@
 def my_name():
     print("My name is KIM Hahyun.")
+    
+def my_info():
+    print("Hello, my nams is KIM Hahyun, and this is my first project!")
---
+++ tests/test_info.py
@@ -0,0 +1,4 @@
+from hello_hxhkim.who import my_info
+
+def test_my_info():
+    my_info()
